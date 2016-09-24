3b9aadb8a8deb4d1bdf0f598a82fe9d0a2464dd5
Felipe Rocha <feliperocha.ti@gmail.com>
2016-09-24 18:41:55 -0300

    implement getNeighbors

--- src/main/groovy/com/feliperocha/gameoflife/domains/Cell.groovy
+++ src/main/groovy/com/feliperocha/gameoflife/domains/Cell.groovy
@@ -9,15 +9,47 @@ class Cell {
     void die() {
         this.alive = false
     }
+
     void live() {
         this.alive = true
     }
 
-
     ArrayList<Cell> getNeighbors(Cell cell, Cell[][] cells) {
         def neighbors = new ArrayList<Cell>()
-
-
+        //Above columns
+        if (cell.y > 0) {
+            //Above left
+            if (cell.x > 0) {
+                neighbors.add(cells[cell.y - 1][cell.x - 1])
+            }
+            //Above
+            neighbors.add(cells[cell.y - 1][cell.x])
+            //Above right
+            if (cell.x < cells[0].length - 1) {
+                neighbors.add(cells[cell.y - 1][cell.x + 1])
+            }
+        }
+        //Inline Left
+        if (cell.x > 0) {
+            neighbors.add(cells[cell.y][cell.x - 1])
+        }
+        //Inline Right
+        if (cell.x < cells[0].length - 1) {
+            neighbors.add(cells[cell.y][cell.x + 1])
+        }
+        //Below columns
+        if (cell.y < cells.length) {
+            //Below left
+            if (cell.x > 0) {
+                neighbors.add(cells[cell.y + 1][cell.x - 1])
+            }
+            //Below
+            neighbors.add(cells[cell.y + 1][cell.x])
+            //Below right
+            if (cell.x < cells[0].length) {
+                neighbors.add(cells[cell.y + 1][cell.x + 1])
+            }
+        }
         neighbors
     }
 
