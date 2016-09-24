32386a26360faa4a82880ecb45c19643aee47314
Felipe Rocha <feliperocha.ti@gmail.com>
2016-09-24 17:04:47 -0300

    add createNextGeneration method in GameOfLifeService

--- src/main/groovy/com/feliperocha/gameoflife/controllers/GameOfLifeController.groovy
+++ src/main/groovy/com/feliperocha/gameoflife/controllers/GameOfLifeController.groovy
@@ -1,8 +1,11 @@
 package com.feliperocha.gameoflife.controllers
 
+import com.feliperocha.gameoflife.domains.Cell
 import com.feliperocha.gameoflife.services.GameOfLifeService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
 
 
@@ -11,5 +14,10 @@ import org.springframework.web.bind.annotation.RestController
 class GameOfLifeController {
 
     @Autowired GameOfLifeService gameOfLifeService
+
+    @RequestMapping(method=RequestMethod.PUT)
+    def runStep(@RequestBody Cell[][] cells) {
+        gameOfLifeService.createNextGeneration(cells)
+    }
 
 }
