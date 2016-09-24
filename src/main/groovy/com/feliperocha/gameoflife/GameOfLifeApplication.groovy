a45ac55e8be17457d49fd909c8632e1bfaa7f151
Felipe Rocha <feliperocha.ti@gmail.com>
2016-09-24 15:45:32 -0300

    first commit

---
+++ src/main/groovy/com/feliperocha/gameoflife/GameOfLifeApplication.groovy
@@ -0,0 +1,12 @@
+package com.feliperocha.gameoflife
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class GameOfLifeApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run GameOfLifeApplication, args
+	}
+}
