61267c9d48d6ac9697a52415370d75f184bcc1cb
Felipe Rocha <feliperocha.ti@gmail.com>
2016-09-24 21:31:43 -0300

    add lasts tests and finishing project

--- src/test/groovy/com/feliperocha/gameoflife/CellTests.groovy
+++ src/test/groovy/com/feliperocha/gameoflife/CellTests.groovy
@@ -79,5 +79,45 @@ class CellTests {
         cell.checkIfLiveOrDie(cell, neighbors)
         assert !cell.alive
     }
+
+    @Test
+    void borderEdgeCellShouldHaveThreeNeighbors() {
+        def cells = new Cell[2][2]
+        cells[0][0] = new Cell(y: 0, x: 0, alive: false)
+        cells[0][1] = new Cell(y: 0, x: 1, alive: false)
+        cells[1][0] = new Cell(y: 1, x: 0, alive: false)
+        cells[1][1] = new Cell(y: 1, x: 1, alive: false)
+        def neighbors = cells[0][0].getNeighbors(cells[0][0], cells)
+        assert neighbors.size().equals(3)
+    }
+
+    @Test
+    void borderLeftOrRightCellShouldHaveFiveNeighbors() {
+        def cells = new Cell[3][2]
+        cells[0][0] = new Cell(y: 0, x: 0, alive: false)
+        cells[0][1] = new Cell(y: 0, x: 1, alive: false)
+        cells[1][0] = new Cell(y: 1, x: 0, alive: false)
+        cells[1][1] = new Cell(y: 1, x: 1, alive: false)
+        cells[2][0] = new Cell(y: 2, x: 0, alive: false)
+        cells[2][1] = new Cell(y: 2, x: 1, alive: false)
+        def neighbors = cells[1][0].getNeighbors(cells[1][0], cells)
+        assert neighbors.size().equals(5)
+    }
+
+    @Test
+    void commonCellShouldHaveEightNeighbors() {
+        def cells = new Cell[3][3]
+        cells[0][0] = new Cell(y: 0, x: 0, alive: false)
+        cells[0][1] = new Cell(y: 0, x: 1, alive: false)
+        cells[0][2] = new Cell(y: 0, x: 2, alive: false)
+        cells[1][0] = new Cell(y: 1, x: 0, alive: false)
+        cells[1][1] = new Cell(y: 1, x: 1, alive: false)
+        cells[1][2] = new Cell(y: 1, x: 2, alive: false)
+        cells[2][0] = new Cell(y: 2, x: 0, alive: false)
+        cells[2][1] = new Cell(y: 2, x: 1, alive: false)
+        cells[2][2] = new Cell(y: 2, x: 2, alive: false)
+        def neighbors = cells[1][1].getNeighbors(cells[1][1], cells)
+        assert neighbors.size().equals(8)
+    }
 }
 
